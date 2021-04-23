9653720f3326fce321dec50f151c68b8c41ee9a2
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2021-04-24 01:05:40 +0300

    Setup application

---
+++ src/Command/PublishCommand.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of modstore publisher package.
+ * For complete copyright and license information, see the LICENSE file
+ * found in the top-level directory of this distribution.
+ **/
+
+declare(strict_types = 1);
+
+namespace Alroniks\Publisher\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+class PublishCommand extends Command
+{
+
+}
